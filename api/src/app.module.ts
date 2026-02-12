b0915ede9fb6fc7d2e2bf0429f48b9251d0b7c0e
nlabrazi <na.labrazi@gmail.com>
2026-02-12 01:44:22 +0100

    fix(netlify): proxy /exchange to railway

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,16 +2,9 @@ import { Module } from '@nestjs/common';
 import { ExchangeController } from './exchange/exchange.controller';
 import { ExchangeService } from './exchange/exchange.service';
 import { HealthController } from './health/health.controller';
-// import { ServeStaticModule } from '@nestjs/serve-static';
-// import { join } from 'path';
 
 @Module({
   imports: [],
-  // imports: [
-  //   ServeStaticModule.forRoot({
-  //     rootPath: join(__dirname, '..', '..', 'ui'),
-  //   }),
-  // ],
   controllers: [ExchangeController, HealthController],
   providers: [ExchangeService],
 })
